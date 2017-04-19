70bf646b5cf920efbf358bea33aeece289fc4b0c
Alexander Volkov <codyfet@mail.ru>
2017-04-19 13:04:09 +0300

    Set build folder to gh-pages

--- client/index.js
+++
@@ -1,5 +0,0 @@
-import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
-import './css/styles.css';
-import 'react-select/dist/react-select.css';
-import 'react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
-import './main';
